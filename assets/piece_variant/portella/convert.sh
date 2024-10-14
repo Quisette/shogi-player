9980610e1364118cada8cb2248b79fe78f9e7c84
Quisette Chung <74784464+Quisette@users.noreply.github.com>
2024-10-14 12:20:35 +0800

    change all assets to webp

--- assets/piece_variant/portella/convert.sh
+++ assets/piece_variant/portella/convert.sh
@@ -1,32 +1,126 @@
-#!/bin/sh
-cp original/0OU.png BK0.png
-cp original/0HI.png BR0.png
-cp original/0KA.png BB0.png
-cp original/0KI.png BG0.png
-cp original/0GI.png BS0.png
-cp original/0KE.png BN0.png
-cp original/0KY.png BL0.png
-cp original/0FU.png BP0.png
-cp original/0GY.png BX0.png
-cp original/0RY.png BR1.png
-cp original/0UM.png BB1.png
-cp original/0NG.png BS1.png
-cp original/0NK.png BN1.png
-cp original/0NY.png BL1.png
-cp original/0TO.png BP1.png
+# #!/bin/sh
+# cp original/0gy.png BK0.png
+# cp original/0hi.png BR0.png
+# cp original/0ka.png BB0.png
+# cp original/0ki.png BG0.png
+# cp original/0gi.png BS0.png
+# cp original/0ke.png BN0.png
+# cp original/0ky.png BL0.png
+# cp original/0fu.png BP0.png
+# cp original/0gy.png BX0.png
+# cp original/0ry.png BR1.png
+# cp original/0um.png BB1.png
+# cp original/0ng.png BS1.png
+# cp original/0nk.png BN1.png
+# cp original/0ny.png BL1.png
+# cp original/0to.png BP1.png
 
-cp original/1OU.png WK0.png
-cp original/1HI.png WR0.png
-cp original/1KA.png WB0.png
-cp original/1KI.png WG0.png
-cp original/1GI.png WS0.png
-cp original/1KE.png WN0.png
-cp original/1KY.png WL0.png
-cp original/1FU.png WP0.png
-cp original/1GY.png WX0.png
-cp original/1RY.png WR1.png
-cp original/1UM.png WB1.png
-cp original/1NG.png WS1.png
-cp original/1NK.png WN1.png
-cp original/1NY.png WL1.png
-cp original/1TO.png WP1.png
+# cp original/1gy.png WK0.png
+# cp original/1hi.png WR0.png
+# cp original/1ka.png WB0.png
+# cp original/1ki.png WG0.png
+# cp original/1gi.png WS0.png
+# cp original/1ke.png WN0.png
+# cp original/1ky.png WL0.png
+# cp original/1fu.png WP0.png
+# cp original/1gy.png WX0.png
+# cp original/1ry.png WR1.png
+# cp original/1um.png WB1.png
+# cp original/1ng.png WS1.png
+# cp original/1nk.png WN1.png
+# cp original/1ny.png WL1.png
+# cp original/1to.png WP1.png
+
+#!/bin/sh
+
+# Copy and convert black pieces
+cp original/0gy.png BK0.png
+cwebp BK0.png -o BK0.webp
+
+cp original/0hi.png BR0.png
+cwebp BR0.png -o BR0.webp
+
+cp original/0ka.png BB0.png
+cwebp BB0.png -o BB0.webp
+
+cp original/0ki.png BG0.png
+cwebp BG0.png -o BG0.webp
+
+cp original/0gi.png BS0.png
+cwebp BS0.png -o BS0.webp
+
+cp original/0ke.png BN0.png
+cwebp BN0.png -o BN0.webp
+
+cp original/0ky.png BL0.png
+cwebp BL0.png -o BL0.webp
+
+cp original/0fu.png BP0.png
+cwebp BP0.png -o BP0.webp
+
+cp original/0gy.png BX0.png
+cwebp BX0.png -o BX0.webp
+
+cp original/0ry.png BR1.png
+cwebp BR1.png -o BR1.webp
+
+cp original/0um.png BB1.png
+cwebp BB1.png -o BB1.webp
+
+cp original/0ng.png BS1.png
+cwebp BS1.png -o BS1.webp
+
+cp original/0nk.png BN1.png
+cwebp BN1.png -o BN1.webp
+
+cp original/0ny.png BL1.png
+cwebp BL1.png -o BL1.webp
+
+cp original/0to.png BP1.png
+cwebp BP1.png -o BP1.webp
+
+# Copy and convert white pieces
+cp original/1gy.png WK0.png
+cwebp WK0.png -o WK0.webp
+
+cp original/1hi.png WR0.png
+cwebp WR0.png -o WR0.webp
+
+cp original/1ka.png WB0.png
+cwebp WB0.png -o WB0.webp
+
+cp original/1ki.png WG0.png
+cwebp WG0.png -o WG0.webp
+
+cp original/1gi.png WS0.png
+cwebp WS0.png -o WS0.webp
+
+cp original/1ke.png WN0.png
+cwebp WN0.png -o WN0.webp
+
+cp original/1ky.png WL0.png
+cwebp WL0.png -o WL0.webp
+
+cp original/1fu.png WP0.png
+cwebp WP0.png -o WP0.webp
+
+cp original/1gy.png WX0.png
+cwebp WX0.png -o WX0.webp
+
+cp original/1ry.png WR1.png
+cwebp WR1.png -o WR1.webp
+
+cp original/1um.png WB1.png
+cwebp WB1.png -o WB1.webp
+
+cp original/1ng.png WS1.png
+cwebp WS1.png -o WS1.webp
+
+cp original/1nk.png WN1.png
+cwebp WN1.png -o WN1.webp
+
+cp original/1ny.png WL1.png
+cwebp WL1.png -o WL1.webp
+
+cp original/1to.png WP1.png
+cwebp WP1.png -o WP1.webp
---
+++ assets/piece_variant/zuan/convert_1.sh
@@ -0,0 +1,64 @@
+# #!/bin/sh
+
+
+# Copy and convert black pieces
+cwebp BK0.png -o BK0.webp
+
+cwebp BR0.png -o BR0.webp
+
+cwebp BB0.png -o BB0.webp
+
+cwebp BG0.png -o BG0.webp
+
+cwebp BS0.png -o BS0.webp
+
+cwebp BN0.png -o BN0.webp
+
+cwebp BL0.png -o BL0.webp
+
+cwebp BP0.png -o BP0.webp
+
+cwebp BX0.png -o BX0.webp
+
+cwebp BR1.png -o BR1.webp
+
+cwebp BB1.png -o BB1.webp
+
+cwebp BS1.png -o BS1.webp
+
+cwebp BN1.png -o BN1.webp
+
+cwebp BL1.png -o BL1.webp
+
+cwebp BP1.png -o BP1.webp
+
+# Copy and convert white pieces
+cwebp WK0.png -o WK0.webp
+
+cwebp WR0.png -o WR0.webp
+
+cwebp WB0.png -o WB0.webp
+
+cwebp WG0.png -o WG0.webp
+
+cwebp WS0.png -o WS0.webp
+
+cwebp WN0.png -o WN0.webp
+
+cwebp WL0.png -o WL0.webp
+
+cwebp WP0.png -o WP0.webp
+
+cwebp WX0.png -o WX0.webp
+
+cwebp WR1.png -o WR1.webp
+
+cwebp WB1.png -o WB1.webp
+
+cwebp WS1.png -o WS1.webp
+
+cwebp WN1.png -o WN1.webp
+
+cwebp WL1.png -o WL1.webp
+
+cwebp WP1.png -o WP1.webp
